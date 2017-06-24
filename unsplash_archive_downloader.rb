561727b567b09e4ceca1769cb13f174a3a142804
Benedict Aluan <benedict.aluan@gmail.com>
2017-06-24 22:33:56 +1200

    Remove Gemfile

--- unsplash_archive_downloader.rb
+++ unsplash_archive_downloader.rb
@@ -1,7 +1,6 @@
-require 'rest-client'
-
 class UnsplashArchiveDownloader
   def call
+    # Adjust the archive loop
     (1..163).each do |archive_num|
       system("wget https://download.unsplash.com/zip-archives/#{archive_num}-archive.zip")
     end
